5c059bfed51ac55198999ff706973213b1e0cab8
Gary <zipe.daden@gmail.com>
2018-04-25 16:29:46 +0800

    Add Login page
    Add custom login page.

--- oauth-server/src/main/java/com/localhost/security/config/SecurityConfig.java
+++ oauth-server/src/main/java/com/localhost/security/config/SecurityConfig.java
@@ -6,20 +6,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.localhost.security.service.UserDetailsService;
+import com.localhost.service.impl.LoginSuccessHandler;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
+    @Bean
+    public LoginSuccessHandler loginSuccessHandler(){
+        return new LoginSuccessHandler();
+    }
+    
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -28,14 +36,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.requestMatchers()
-            .antMatchers("/login", "/oauth/authorize")
+        http.csrf().disable()
+            .requestMatchers()
+            .antMatchers("/", "/login**", "/oauth/authorize", "/oauth/confirm_access")
             .and()
             .authorizeRequests()
             .anyRequest()
             .authenticated()
             .and()
             .formLogin()
+            .loginPage("/login")
+            .usernameParameter("username")
+            .passwordParameter("password")
             .permitAll();
     }
 
