3243a438bf17a0bb0da25a922b4ce648e559b917
Gary <zipe.daden@gmail.com>
2018-04-30 16:08:14 +0800

    Change methods name Change controller methods name.

--- oauth-jwt-client/src/main/java/com/localhost/controller/TestController.java
+++ oauth-jwt-client/src/main/java/com/localhost/controller/TestController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
 	@GetMapping("/jwt1")
-    public String test1() {
+    public String jwt1() {
         return "jwt1";
     }
 
 	@GetMapping("/jwt2")
-    public String test2() {
+    public String jwt2() {
         return "jwt2";
     }
 
 	@GetMapping("/jwt3")
-    public String test3() {
+    public String jwt3() {
         return "jwt3";
     }
 
 	@GetMapping("/jwt4")
-    public String test4() {
+    public String jwt4() {
         return "jwt4";
     }
 
