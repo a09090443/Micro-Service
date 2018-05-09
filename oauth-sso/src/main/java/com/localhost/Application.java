1e93e194e8e86cdffb30c6da5c9232fd65e92f1e
Gary <zipe.daden@gmail.com>
2018-05-09 22:05:25 +0800

    Fix security issue
    Fix spring security didn't work issue.

--- oauth-sso/src/main/java/com/localhost/Application.java
+++ oauth-sso/src/main/java/com/localhost/Application.java
@@ -1,14 +1,12 @@
 package com.localhost;
 
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = Application.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,classes = Application.class)
 public class Application {
 	
     @Bean
