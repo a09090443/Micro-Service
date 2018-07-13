bd0e13f80111ebb71f84e5001c75480e4de60523
Gary <zipe.daden@gmail.com>
2018-07-13 18:21:51 +0800

    Move old jar file.
    Rename old jar file and move to "~/backup_jar" directory.

--- script/micro-service.sh
+++ script/micro-service.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 
+OIFS="$IFS"
+IFS=':'
 read -a new_string <<< "$1"
+IFS="$OIFS"
 
 project_name=${new_string[0]}
 port=${new_string[1]}
 version=$2
 
 jar_file_name="$project_name-$version".jar
+old_jar_file=""
 source_dir=~/micro-service/
 target_dir=~/deploy/
 project_dir_path=$target_dir$project_name
+backup_dir=~/backup_jar/
 
 file_full_path="$source_dir$jar_file_name"
+time=$(date +"%Y%m%d_%H%M%S")
 
 echo "Jar file : $jar_file_name"
 echo "Source directory : $source_dir"
 
 if [ -d "$project_dir_path" ]
 then
+       old_jar_file=`find "$project_dir_path" -type f -name "$project_name*"`
+       echo "Old jar file name : $old_jar_file"
+       echo "Move to $backup_dir"
+       /bin/cp -Ra "$old_jar_file" "$backup_dir"$project_name"_"$time".jar"
        echo "Remove $project_dir_path directory files."
        /bin/rm -rf "$project_dir_path"/*
 else
@@ -28,10 +38,10 @@ fi
 
 if [ -f "$file_full_path" ]
 then
-        echo "$file_full_path found."
+	echo "$file_full_path found."
         /bin/cp -Ra "$file_full_path" "$project_dir_path"
 else
-        echo "$file_full_path not found."
+	echo "$file_full_path not found."
         exit 1
 fi
 
