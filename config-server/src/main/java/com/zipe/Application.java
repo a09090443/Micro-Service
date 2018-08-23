670db423235a66897ab06159f4dc7bf4ebe5aa01
Gary <zipe.daden@gmail.com>
2018-08-24 06:13:22 +0800

    Add refresh function
    update Url "http://localhost:8888/refresh" and Http method is "POST".

--- config-server/src/main/java/com/zipe/Application.java
+++ config-server/src/main/java/com/zipe/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableConfigServer
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
