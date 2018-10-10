7f60853aa9687cff938c8ccf04d9ba139d582a94
Gary <zipe.daden@gmail.com>
2018-10-10 21:16:03 +0800

    Add id column.

--- eureka-server/src/main/java/com/zipe/model/UserInfo.java
+++ eureka-server/src/main/java/com/zipe/model/UserInfo.java
@@ -1,299 +1,224 @@
 package com.zipe.model;
 
 import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import java.io.Serializable;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "user_info")
-public class UserInfo implements Serializable{
-	static final long serialVersionUID = 1L;
-	
-	@Id
-	@NotEmpty
-	@Column(name = "user_id", nullable = false, length = 6, unique=true)
-	private String userId;
-
-    @Column(name = "login_id", updatable = false, nullable = false, length = 50)
-    @NotNull
-    @Size(max = 50)
+public class UserInfo {
+    private int id;
+    private String userId;
     private String loginId;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String address;
+    private String birthday;
+    private String image;
+    private String phone;
+    private boolean activated;
+    private String registerTime;
+    private Set<Authority> authorities;
+    private Set<PersonalTitle> personalTitle;
 
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "user_id", nullable = false, length = 6, unique=true)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "login_id", updatable = false, nullable = false, length = 50)
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    @Basic
     @Column(name = "password", updatable = false, nullable = false, length = 500)
     @NotNull
     @Size(max = 80)
-    private String password;
-
-	@Column(name = "first_name", nullable = false, length = 50)
-	private String firstName;
-
-	@Column(name = "last_name", length = 50)
-	private String lastName;
-
-    @Column(name = "email", updatable = false, nullable = false, length = 50)
-    @Email
-    @Size(max = 50)
-    private String email;
-    
-    private boolean activated;
-
-	@Column(name = "image", length = 20)
-	private String image;
-
-	@Column(name = "birthday")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private String birthday;
-
-	@Column(name = "address", length = 40)
-	private String address;
-
-	@Column(name = "phone", length = 10)
-	@Pattern(regexp="(^$|[0-9]{10})")
-	private String phone;
-
-	@Column(name = "registerTime", length = 19)
-	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
-	private String registerTime;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_authority",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "authority_id")
-    )
-    private Set<Authority> authorities;
-
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "user_title",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "title_id")
-    )
-    private Set<PersonalTitle> personalTitle;
-
-    public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getLoginId() {
-		return loginId;
-	}
-
-	public void setLoginId(String loginId) {
-		this.loginId = loginId;
-	}
-
     public String getPassword() {
         return password;
     }
 
-    public UserInfo setPassword(String password) {
+    public void setPassword(String password) {
         this.password = password;
-        return this;
     }
 
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 50)
     public String getFirstName() {
-		return firstName;
-	}
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    @Basic
+    @Column(name = "last_name", length = 50)
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getEmail() {
+    @Basic
+    @Column(name = "email", updatable = false, nullable = false, length = 50)
+    @Email
+    @Size(max = 50)
+    public String getEmail() {
         return email;
     }
 
-    public UserInfo setEmail(String email) {
+    public void setEmail(String email) {
         this.email = email;
-        return this;
     }
 
+    @Basic
+    @Column(name = "address", length = 40)
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Basic
+    @Column(name = "birthday")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    @Basic
+    @Column(name = "image", length = 20)
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Basic
+    @Column(name = "phone", length = 10)
+    @Pattern(regexp="(^$|[0-9]{10})")
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Basic
+    @Column(name = "activated")
     public boolean isActivated() {
         return activated;
     }
 
-    public UserInfo setActivated(boolean activated) {
+    public void setActivated(boolean activated) {
         this.activated = activated;
-        return this;
     }
 
+    @Basic
+    @Column(name = "registerTime", length = 19)
+    @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+    public String getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "authority_id")
+    )
     public Set<Authority> getAuthorities() {
         return authorities;
     }
 
-    public UserInfo setAuthorities(Set<Authority> authorities) {
+    public void setAuthorities(Set<Authority> authorities) {
         this.authorities = authorities;
-        return this;
     }
 
-	public String getImage() {
-		return image;
-	}
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_title",
+            joinColumns = @JoinColumn(name = "id"),
+            inverseJoinColumns = @JoinColumn(name = "title_id")
+    )
+    public Set<PersonalTitle> getPersonalTitle() {
+        return personalTitle;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setPersonalTitle(Set<PersonalTitle> personalTitle) {
+        this.personalTitle = personalTitle;
+    }
 
-	public String getBirthday() {
-		return birthday;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfo that = (UserInfo) o;
+        return activated == that.activated &&
+                Objects.equals(id, that.id) &&
+                Objects.equals(userId, that.userId) &&
+                Objects.equals(loginId, that.loginId) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(address, that.address) &&
+                Objects.equals(birthday, that.birthday) &&
+                Objects.equals(image, that.image) &&
+                Objects.equals(phone, that.phone) &&
+                Objects.equals(registerTime, that.registerTime) &&
+                Objects.equals(authorities, that.authorities) &&
+                Objects.equals(personalTitle, that.personalTitle);
+    }
 
-	public void setBirthday(String birthday) {
-		this.birthday = birthday;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getRegisterTime() {
-		return registerTime;
-	}
-
-	public void setRegisterTime(String registerTime) {
-		this.registerTime = registerTime;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + (activated ? 1231 : 1237);
-		result = prime * result + ((address == null) ? 0 : address.hashCode());
-		result = prime * result + ((authorities == null) ? 0 : authorities.hashCode());
-		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((image == null) ? 0 : image.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-		result = prime * result + ((loginId == null) ? 0 : loginId.hashCode());
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
-		result = prime * result + ((phone == null) ? 0 : phone.hashCode());
-		result = prime * result + ((registerTime == null) ? 0 : registerTime.hashCode());
-		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		UserInfo other = (UserInfo) obj;
-		if (activated != other.activated)
-			return false;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (authorities == null) {
-			if (other.authorities != null)
-				return false;
-		} else if (!authorities.equals(other.authorities))
-			return false;
-		if (birthday == null) {
-			if (other.birthday != null)
-				return false;
-		} else if (!birthday.equals(other.birthday))
-			return false;
-		if (email == null) {
-			if (other.email != null)
-				return false;
-		} else if (!email.equals(other.email))
-			return false;
-		if (firstName == null) {
-			if (other.firstName != null)
-				return false;
-		} else if (!firstName.equals(other.firstName))
-			return false;
-		if (image == null) {
-			if (other.image != null)
-				return false;
-		} else if (!image.equals(other.image))
-			return false;
-		if (lastName == null) {
-			if (other.lastName != null)
-				return false;
-		} else if (!lastName.equals(other.lastName))
-			return false;
-		if (loginId == null) {
-			if (other.loginId != null)
-				return false;
-		} else if (!loginId.equals(other.loginId))
-			return false;
-		if (password == null) {
-			if (other.password != null)
-				return false;
-		} else if (!password.equals(other.password))
-			return false;
-		if (phone == null) {
-			if (other.phone != null)
-				return false;
-		} else if (!phone.equals(other.phone))
-			return false;
-		if (registerTime == null) {
-			if (other.registerTime != null)
-				return false;
-		} else if (!registerTime.equals(other.registerTime))
-			return false;
-		if (userId == null) {
-			if (other.userId != null)
-				return false;
-		} else if (!userId.equals(other.userId))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "UserInfo [userId=" + userId + ", loginId=" + loginId + ", password=" + password + ", firstName="
-				+ firstName + ", lastName=" + lastName + ", email=" + email + ", activated=" + activated + ", image="
-				+ image + ", birthday=" + birthday + ", address=" + address + ", phone=" + phone + ", registerTime="
-				+ registerTime + ", authorities=" + authorities + "]";
-	}
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, loginId, password, firstName, lastName, email, address, birthday, image, phone, activated, registerTime, authorities, personalTitle);
+    }
 }
