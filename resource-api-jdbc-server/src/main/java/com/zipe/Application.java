cace2029ad52d53b63d3c8b41fb006a08b72bb08
Gary <zipe.daden@gmail.com>
2019-10-06 00:14:19 +0800

    Rename api servers name
    Rename api servers name

---
+++ resource-api-jdbc-server/src/main/java/com/zipe/Application.java
@@ -0,0 +1,11 @@
+package com.zipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
